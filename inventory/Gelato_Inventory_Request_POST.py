469f87cc0d26fd89979e0dc51417922bd39de9bc
Yuan Lin <yu.an.lintech@gmail.com>
2023-05-29 22:59:17 +1000

    committed

--- inventory/Gelato_Inventory_Request_POST.py
+++ inventory/Gelato_Inventory_Request_POST.py
@@ -1,15 +1,18 @@
 import requests
-url = 'https://postman-echo.com/post'
 
-data = {
-  "version": "1.0",
-  "SKU": [
-    "001-HOODIE-YEL-XL",
-    "002-HOODIE-YEL-L",
-    "003-HOODIE-YEL-M"
-  ]
-}
-
-headers = {"Content-Type":"application/json"}
-
-r = requests.post(url, data, headers, timeout=5)
+def handler(pd: "pipedream"):
+    url = "https://jsonplaceholder.typicode.com/todos/1"
+    body = {
+        "version": "1.0",
+        "SKU": [
+            "001-HOODIE-YEL-XL",
+            "002-HOODIE-YEL-L",
+            "003-HOODIE-YEL-M"
+        ]
+    }
+    header = {"Content-Type":"application/json"}
+    r = requests.post(url, json=body, headers=header)
+    data = r.json()
+    # print(pd.steps["trigger"]["context"]["id"])
+    # Return data for use in future steps
+    return data
