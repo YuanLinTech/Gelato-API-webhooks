fb9daa51cfc3c4cc84b1c2e9bc5cbe4487ebcb85
Yuan Lin <yu.an.lintech@gmail.com>
2023-05-29 19:27:44 +1000

    committed

--- inventory/Gelato_Inventory_Request_POST.py
+++ inventory/Gelato_Inventory_Request_POST.py
@@ -1,1 +1,16 @@
+import requests
+url = 'https://postman-echo.com/post'
+
+data = {
+  "version": "1.0",
+  "SKU": [
+    "001-HOODIE-YEL-XL",
+    "002-HOODIE-YEL-L",
+    "003-HOODIE-YEL-M"
+  ]
+}
+
+headers = {"Content-Type":"application/json"}
+
+r = requests.post(url, data, headers, timeout=5)
 
