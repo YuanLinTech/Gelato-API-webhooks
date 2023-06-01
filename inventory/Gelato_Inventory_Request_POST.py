b9edb64a500471ea14644a1296355959d6352794
Yuan Lin <yu.an.lintech@gmail.com>
2023-06-01 21:32:19 +1000

    workflow finalised

--- inventory/Gelato_Inventory_Request_POST.py
+++ inventory/Gelato_Inventory_Request_POST.py
@@ -1,7 +1,7 @@
-# Send_HTTP_POST_Request
-
 import requests
+import pipedream
 
+# Send HTTP POST request
 def handler(pd: "pipedream"):
     url = "https://jsonplaceholder.typicode.com/todos/1"
     body = pd.steps["trigger"]["event"]["body"]["SKU"]
@@ -11,3 +11,16 @@ def handler(pd: "pipedream"):
     # print(pd.steps["trigger"]["context"]["id"])
     # Return data for use in future steps
     return product_skus
+
+# Return stock status
+def get_stock_status(product_SKUs):
+    stock_status = {}
+    for sku in product_SKUs:
+        stock_status[sku] = 'In Stock'
+    return stock_status
+
+def handler(pd: "pipedream"):
+    product_skus = pd.steps["Send_HTTP_POST_Request"]["$return_value"]
+    stock_status = get_stock_status(product_skus)
+    response = {'stock_status': stock_status}
+    return response
