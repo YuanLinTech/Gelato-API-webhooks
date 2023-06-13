06c57b4e8a3020c0078cb6eee90131b98a0b28e2
YuanLinTech <66548341+YuanLinTech@users.noreply.github.com>
2023-06-13 13:17:34 +1000

    Updated sandbox.py

--- inventory/sandbox.py
+++ inventory/sandbox.py
@@ -1,5 +1,7 @@
 import csv
+
+SKUlist = []
 with open('NZ_NVJ_Apparel_SKUs_sheet.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in spamreader:
-        print(row)
+         SKUlist.append(row[0])
