a3d142516c07575905ceb01dc8078513d8d3fd96
YuanLinTech <66548341+YuanLinTech@users.noreply.github.com>
2023-06-14 14:09:07 +1000

    Updated app_consumer.py

--- inventory/app_consumer.py
+++ inventory/app_consumer.py
@@ -17,9 +17,10 @@ def render_template_stream(template_name, **context):
 def index():
     return render_template('consumer.html')
 
-# Sending Message through the websocket
+# Send a message through the websocket
+# Emit a SocketIO event back to the client
 def send_message(event, namespace, room, message):
-    # print("Message = ", message)
+    print("Message = ", message)
     socketio.emit(event, message, namespace=namespace, room=room)
 
 # Create a unique session ID and store it within the application configuration file
@@ -27,18 +28,21 @@ sid = str(uuid.uuid4())
 app.config.update({'uid': sid})
 print("initialize_params - Session ID stored =", sid)
 
-# Receive the webhooks and emit websocket events
+# Receive the webhook requests and emit a SocketIO event back to the client
 @app.route('/consumetasks', methods=['POST'])
 def consumetasks():
     if request.method == 'POST':
         data = request.json
         if data:
            print("Received Data = ", data)
-           roomid = app.config['uid'] # Generate Room ID
+           roomid =  app.config['uid']
            var = json.dumps(data)
            send_message(event='msg', namespace='/collectHooks', room=roomid, message=var)
-           print("Consuming tasks")
-           return Response(render_template_stream('consumer.html', stockStatus = tasks_consumer.sendStockStatus()))
+    return 'OK'
+
+            
+           
+return Response(render_template_stream('consumer.html', stockStatus = tasks_consumer.sendStockStatus()))
 
 # Execute on connecting
 @socketio.on('connect', namespace='/collectHooks')
