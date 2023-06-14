08268b6d6426a0f68e669f606ca06cb18e0dc3a2
YuanLinTech <66548341+YuanLinTech@users.noreply.github.com>
2023-06-14 16:11:04 +1000

    Updated tasks_consumer.py

--- inventory/tasks_consumer.py
+++ inventory/tasks_consumer.py
@@ -1,6 +1,19 @@
 import csv
 
 SKUlist = []
+
+# Receive the webhook requests and emit a SocketIO event back to the client
+@app.route('/consumetasks', methods=['POST'])
+def consume_tasks():
+    if request.method == 'POST':
+        data = request.json
+        if data:
+           print("Received Data = ", data)
+           roomid =  app.config['uid']
+           var = json.dumps(data)
+           send_message(event='msg', namespace='/collectHooks', room=roomid, message=var)
+    return 'OK'
+
 def sendStockStatus():
     with open('NZ_NVJ_Apparel_SKUs_sheet.csv', newline='') as csvFile:
         stockReader = csv.reader(csvFile, delimiter=',', quotechar='"')
