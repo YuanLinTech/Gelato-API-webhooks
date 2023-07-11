7876b98259979a45477c598fdef7a301ae468a97
Yuan Lin <yu.an.lintech@gmail.com>
2023-07-11 22:56:31 +1000

    installed jquery module

--- inventory/app_consumer.py
+++ inventory/app_consumer.py
@@ -1,16 +1,16 @@
-from flask import render_template, Response, request
-from flask_socketio import join_room
+from flask import render_template, Response, request, stream_template
 from init_consumer import app, socketio
 import tasks_consumer
 import uuid
 
-# Render a template with a given context as a stream and return a TemplateStream
-def render_template_stream(template_name, **context): # **context means the context must be a dictionary.
-    app.update_template_context(context) # Update the template context with some commonly used variables. 
-    t = app.jinja_env.get_template(template_name) # jinja2.Environment.get_template() # Load a template by name with loader and return a Template.
-    rv = t.stream(context) # jinja2.Template.stream # Return a TemplateStream that returns one function after another as strings
-    rv.enable_buffering(5) # jinja2.environment.TemplateStream.enable_buffering # Buffer 5 items before yielding them
-    return rv # Return a TemplateStream
+# # Render a template with a given context as a stream and return a TemplateStream
+# def render_template_stream(template_name, **context):
+#     print(context)
+#     app.update_template_context(context) # Update the template context with some commonly used variables. 
+#     t = app.jinja_env.get_template(template_name) # jinja2.Environment.get_template() # Load a template by name with loader and return a Template.
+#     rv = t.stream(context) # jinja2.Template.stream # Return a TemplateStream that returns one function after another as strings
+#     rv.enable_buffering(5) # jinja2.environment.TemplateStream.enable_buffering # Buffer 5 items before yielding them
+#     return rv # Return a TemplateStream
 
 # Registers a function to be run before the first request to this instance of the application
 # Create a unique session ID and store it within the application configuration file
@@ -31,7 +31,7 @@ def getStockStatus():
     # Handle the POST request
     if request.method == 'POST':
         print("Retrieving stock status")
-        return Response(render_template_stream('consumer.html', stockInfo = tasks_consumer.sendStockStatus()))     
+        return Response(stream_template('consumer.html', stockInfo = list(tasks_consumer.sendStockStatus())))     
     # Handle the GET request
     elif request.method == 'GET':
         return '''
--- inventory/tasks_consumer.py
+++ inventory/tasks_consumer.py
@@ -3,29 +3,26 @@ from flask import request, stream_with_context
 from init_consumer import app, socketio
 import json
 
-# Receive the webhook requests and emit a SocketIO event back to the client
-def send_message(data):
-    status_code = 0
-    if request.method == 'POST':
-        roomid = app.config['uid']
-        msg = json.dumps(data)
-        event = "Send_stock_status"
-        socketio.emit(event, msg, namespace = '/collectHooks', room = roomid)
-        status_code = 200
-    else:
-        status_code = 405 # Method not allowed
-    return status_code
+# # Receive the webhook requests and emit a SocketIO event back to the client
+# def send_message(data):
+#     status_code = 0
+#     if request.method == 'POST':
+#         roomid = app.config['uid']
+#         msg = json.dumps(data)
+#         event = "Send_stock_status"
+#         socketio.emit(event, msg, namespace = '/collectHooks', room = roomid)
+#         status_code = 200
+#     else:
+#         status_code = 405 # Method not allowed
+#     return status_code
 
 # Retrieve the stock status of the products sent through the webhook requests and return them back to the client. 
-@app.route('/consumetasks', methods=['POST'])
 def sendStockStatus():
     stockList = [] # List of products in stock
     with open("NZ_NVJ_Apparel_SKUs_sheet.csv", newline='') as csvFile:
         stockReader = csv.reader(csvFile, delimiter=',', quotechar='"')
         for row in stockReader:
             stockList.append(row[0])
-    
-    # stockSheet = {} # Dictionary of products sent in the request and their stock status
 
     def generateStockStatus():
         request_data = request.get_json()
@@ -40,8 +37,7 @@ def sendStockStatus():
                     else:
                         stockStatus = "Out of Stock"
                         stockSheet.update({str(stock):stockStatus})
-
-                    send_message(stockSheet)
+                    # send_message(stockSheet)
                     yield str(stock)
                     yield stockStatus
                         
